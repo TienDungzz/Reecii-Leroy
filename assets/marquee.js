a294a49f92988d8bb7b1e1f4110681959583ea79
demothemevale <demothemevale@halothemes.com>
2025-08-09 15:09:55 +0700

    horizon commit

--- assets/marquee.js
+++ assets/marquee.js
@@ -1,3 +1,6 @@
+import { Component } from '@theme/component';
+import { debounce } from '@theme/utilities';
+
 const ANIMATION_OPTIONS = {
   duration: 500,
 };
@@ -11,29 +14,29 @@ const ANIMATION_OPTIONS = {
  *
  * @extends Component<Refs>
  */
-class MarqueeComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.wrapper = this.querySelector(".marquee__wrapper");
-    this.content = this.querySelector(".marquee__content");
-  }
+class MarqueeComponent extends Component {
+  requiredRefs = ['wrapper', 'content'];
 
   connectedCallback() {
-    if (this.content.firstElementChild?.children.length === 0) return;
+    super.connectedCallback();
+
+    const { content } = this.refs;
+    if (content.firstElementChild?.children.length === 0) return;
 
     this.#addRepeatedItems();
     this.#duplicateContent();
     this.#setSpeed();
 
-    window.addEventListener("resize", this.#handleResize);
-    this.addEventListener("pointerenter", this.#slowDown);
-    this.addEventListener("pointerleave", this.#speedUp);
+    window.addEventListener('resize', this.#handleResize);
+    this.addEventListener('pointerenter', this.#slowDown);
+    this.addEventListener('pointerleave', this.#speedUp);
   }
 
   disconnectedCallback() {
-    window.removeEventListener("resize", this.#handleResize);
-    this.removeEventListener("pointerenter", this.#slowDown);
-    this.removeEventListener("pointerleave", this.#speedUp);
+    super.disconnectedCallback();
+    window.removeEventListener('resize', this.#handleResize);
+    this.removeEventListener('pointerenter', this.#slowDown);
+    this.removeEventListener('pointerleave', this.#speedUp);
   }
 
   /**
@@ -44,7 +47,7 @@ class MarqueeComponent extends HTMLElement {
   #slowDown = debounce(() => {
     if (this.#animation) return;
 
-    const animation = this.wrapper.getAnimations()[0];
+    const animation = this.refs.wrapper.getAnimations()[0];
 
     if (!animation) return;
 
@@ -62,7 +65,7 @@ class MarqueeComponent extends HTMLElement {
   #speedUp() {
     this.#slowDown.cancel();
 
-    const animation = this.wrapper.getAnimations()[0];
+    const animation = this.refs.wrapper.getAnimations()[0];
 
     if (!animation || animation.playbackRate === 1) return;
 
@@ -81,25 +84,27 @@ class MarqueeComponent extends HTMLElement {
   }
 
   get clonedContent() {
-    const lastChild = this.wrapper.lastElementChild;
+    const { content, wrapper } = this.refs;
+    const lastChild = wrapper.lastElementChild;
 
-    return this.content !== lastChild ? lastChild : null;
+    return content !== lastChild ? lastChild : null;
   }
 
   #setSpeed(value = this.#calculateSpeed()) {
-    this.style.setProperty("--marquee-speed", `${value}s`);
+    this.style.setProperty('--marquee-speed', `${value}s`);
   }
 
   #calculateSpeed() {
-    const speedFactor = Number(this.getAttribute("data-speed-factor"));
+    const speedFactor = Number(this.getAttribute('data-speed-factor'));
     const marqueeWidth = this.offsetWidth;
     const speed = Math.ceil(marqueeWidth / speedFactor / 2);
     return speed;
   }
 
   #handleResize = debounce(() => {
+    const { content } = this.refs;
     const newNumberOfCopies = this.#calculateNumberOfCopies();
-    const currentNumberOfCopies = this.content.children.length;
+    const currentNumberOfCopies = content.children.length;
 
     if (newNumberOfCopies > currentNumberOfCopies) {
       this.#addRepeatedItems(newNumberOfCopies - currentNumberOfCopies);
@@ -113,7 +118,7 @@ class MarqueeComponent extends HTMLElement {
   }, 250);
 
   #restartAnimation() {
-    const animations = this.wrapper.getAnimations();
+    const animations = this.refs.wrapper.getAnimations();
 
     requestAnimationFrame(() => {
       for (const animation of animations) {
@@ -123,45 +128,43 @@ class MarqueeComponent extends HTMLElement {
   }
 
   #duplicateContent() {
-
     this.clonedContent?.remove();
 
-    const clone = /** @type {HTMLElement} */ (
-      this.content.cloneNode(true)
-    );
+    const clone = /** @type {HTMLElement} */ (this.refs.content.cloneNode(true));
 
-    clone.setAttribute("aria-hidden", "true");
+    clone.setAttribute('aria-hidden', 'true');
+    clone.removeAttribute('ref');
 
-    this.wrapper.appendChild(clone);
+    this.refs.wrapper.appendChild(clone);
   }
 
   #addRepeatedItems(numberOfCopies = this.#calculateNumberOfCopies()) {
-    if (!this.wrapper) return;
+    const { content } = this.refs;
+    const wrapper = content.firstElementChild;
+
+    if (!wrapper) return;
 
     for (let i = 0; i < numberOfCopies - 1; i++) {
-      const clone = this.wrapper.querySelector('.marquee__repeated-items').cloneNode(true);
-
-      this.content.appendChild(clone);
+      const clone = wrapper.cloneNode(true);
+      content.appendChild(clone);
     }
   }
 
   #removeRepeatedItems(numberOfCopies = this.#calculateNumberOfCopies()) {
+    const { content } = this.refs;
 
     for (let i = 0; i < numberOfCopies; i++) {
-      this.content.lastElementChild?.remove();
+      content.lastElementChild?.remove();
     }
   }
 
   #calculateNumberOfCopies() {
+    const { content } = this.refs;
     const marqueeWidth = this.offsetWidth;
     const marqueeRepeatedItemWidth =
-      this.content.firstElementChild instanceof HTMLElement
-        ? this.content.firstElementChild.offsetWidth
-        : 1;
+      content.firstElementChild instanceof HTMLElement ? content.firstElementChild.offsetWidth : 1;
 
-    return marqueeRepeatedItemWidth === 0
-      ? 1
-      : Math.ceil(marqueeWidth / marqueeRepeatedItemWidth);
+    return marqueeRepeatedItemWidth === 0 ? 1 : Math.ceil(marqueeWidth / marqueeRepeatedItemWidth);
   }
 }
 
@@ -176,14 +179,7 @@ class MarqueeComponent extends HTMLElement {
  * @param {function(number): number} [params.easing] - The easing function.
  * @param {function(): void} [params.onComplete] - The function to call when the animation completes.
  */
-function animateValue({
-  from,
-  to,
-  duration,
-  onUpdate,
-  easing = (t) => t * t * (3 - 2 * t),
-  onComplete,
-}) {
+function animateValue({ from, to, duration, onUpdate, easing = (t) => t * t * (3 - 2 * t), onComplete }) {
   const startTime = performance.now();
   let cancelled = false;
   let currentValue = from;
@@ -203,7 +199,7 @@ function animateValue({
 
     if (progress < 1) {
       requestAnimationFrame(animate);
-    } else if (typeof onComplete === "function") {
+    } else if (typeof onComplete === 'function') {
       onComplete();
     }
   }
@@ -220,4 +216,6 @@ function animateValue({
   };
 }
 
-customElements.define("marquee-component", MarqueeComponent);
+if (!customElements.get('marquee-component')) {
+  customElements.define('marquee-component', MarqueeComponent);
+}
