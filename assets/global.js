51e8e8751b84929564786332270eea9b5fabbe60
Halothemes <demothemevale@halothemes.com>
2025-08-18 10:26:26 +0700

    Hyun - 18/08/2025

--- assets/global.js
+++ assets/global.js
@@ -1365,6 +1365,7 @@ class SwiperComponent extends HTMLElement {
         disableOnInteraction: false,
       },
       slidesPerView: getOption("slides-per-view", 1),
+      centeredSlides: getOption("centered-slides", false),
       autoHeight: getOption("auto-height", false),
       navigation: {
         nextEl: this.swiperEl.querySelector(".swiper-button-next"),
